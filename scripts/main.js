f05704d4a475593496e027f173d022abb4b30b14
Alex <aleexsantos1509@gmail.com>
2023-10-10 10:53:08 +0200

    Area and perimeter calculator

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+/**
+ * Autor: Alejandro Santos Garcia
+ * Github repository: 
+ */
+
+const area = document.getElementById("area");
+const perimeter = document.getElementById("perimeter");
+
+const calculateBtn = document.getElementById("calculate");
+
+calculateBtn.addEventListener("click", () => {
+  const height = parseFloat(document.getElementById("height").value);
+  const width = parseFloat(document.getElementById("width").value);
+
+  let areaResult = height * width;
+  let perimeterResult = 2 * (height + width);
+
+  area.textContent = areaResult;
+  perimeter.textContent = perimeterResult;
+});
